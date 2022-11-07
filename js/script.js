d438437ce5709090f59a3b4f52425d9ce98d37e5
DruckDeveloper <druckdev@gmail.com>
2022-11-07 15:52:18 -0500

    Comments added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 //Declaramos variables
+//Estas variables globales se utilizan al momento de realizar una operacion 
 var operandoa;
 var operandob;
 var operacion;
 
+/*Se declara una funcion que trae por id los elementos del documento HTML con los que se va a interactuar 
+Esta funcion se ejecuta por medio del evento onload que se encuentra en el body
+*/
     function init(){
     //variables
     var resultado = document.getElementById('resultado');
@@ -26,6 +30,7 @@ var operacion;
 
 
 //Eventos de click
+//Cada vez que se produce un evento de click se ejecuta una funcion que modifica la zona donde la calculadora muestra el resultado y los numeros que el usuario pulso 
 uno.onclick = function(e){
     resultado.textContent = resultado.textContent  + "1";
 }
@@ -84,10 +89,12 @@ igual.onclick = function(e){
     resolver();
 }
 
-
+//Esta funcion elimina el contenido de la secciion de resultado de la calculadora 
 function limpiar(){
     resultado.textContent = "";
 }
+
+//Esta funcion deja las variables de operacion en 0, para volver a ser utilizadas en una nueva operacion 
 function resetear(){
     resultado.textContent = "";
     operandoa = 0;
@@ -95,7 +102,9 @@ function resetear(){
     operacion = "";
 }
 
-
+/*Esta funcion ejecuta un trozo de codigo dependiendo de la operacion realizada 
+El resultado se guardara en la variable res y esta misma se mostrara en el documento HTML ne la seccion del resultado
+*/
 function resolver(){
     var res = 0;
     switch(operacion){
